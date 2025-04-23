396193b09bff5d445a380ea2297b68fca201c013
R-azy <muhammedrazy1798@gmail.com>
2025-04-23 11:32:20 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import DestinationContainer from "./components/DestinationContainer";
+import "/styles.css";
 function App() {
   return (
-    <div>
-      {/* Your code here */}
-    </div>
-  )
+    <>
+      <div>
+        {/* Your code here */}
+        <DestinationContainer></DestinationContainer>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
